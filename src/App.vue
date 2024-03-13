f455c80fb31bf45b016f4fdc4be0abfc8a1ee4ec
Richiisontheway <mineiriccardo@gmail.com>
2024-03-13 16:01:10 +0100

    init vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "helo",
+    };
+  },
+};
+</script>
+
+<template>
+  <header>header</header>
+  <main>
+    <h1>Titolo {{ message }}</h1>
+  </main>
+  <footer>Footer</footer>
+</template>
+
+<style scoped></style>
