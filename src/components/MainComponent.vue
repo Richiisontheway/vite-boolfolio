3a0f1a2557ba90daf95eba3da57ba706caab32c0
Richiisontheway <mineiriccardo@gmail.com>
2024-03-13 19:01:39 +0100

    add Projectcard manca lo style

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,34 +1,68 @@
 <script>
+import ProjectCard from "./ProjectCard.vue";
 import axios from "axios";
 export default {
-    data() {
-        return {
-        projects: [],
-        };
-    },
-    created() {
-        axios.get("http://127.0.0.1:8000/api/projects").then((res) => {
-        console.log(res.data);
-        this.projects = res.data.result.data;
+  data() {
+    return {
+      projects: [],
+      currentPage: 1,
+      lastPage: 1,
+    };
+  },
+  created() {
+    this.getProjects(this.currentPage);
+  },
+  methods: {
+    getProjects(page) {
+      axios
+        .get("http://127.0.0.1:8000/api/projects", {
+          params: {
+            page: page,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          this.projects = res.data.result.data;
+          this.currentPage = res.data.result.current_page;
+          this.lastPage = res.data.result.last_page;
         });
     },
+    prevPage() {
+      if (this.current_page > 1) {
+        this.getProjects(this.currentPage - 1);
+      }
+    },
+    nextPage() {
+      if (this.current_page < this.lastPage) {
+        this.getProjects(this.currentPage + 1);
+      }
+    },
+  },
+  components: {
+    ProjectCard,
+  },
 };
 </script>
 
 <template>
-    <main>
-        <div>
-            <div v-for="project in projects" :key="projects.id">
-                <ul>
-                    {{ project.title }}
-                </ul>
-            </div>
-        </div>
-    </main>
+  <main>
+    <div class="container">
+      <ProjectCard
+        v-for="project in projects"
+        :key="projects.id"
+        :project="project"
+      />
+    </div>
+    <div class="nav_container">
+      <button @click="prevPage">Precedente</button>
+      <button @click="nextPage">Successivo</button>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-main {
-  color: red;
+.container {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  props: {
+    project: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card-container">
+    <div class="info-container">
+      <div>
+        Titolo:
+        <h1>
+          {{ project.title }}
+        </h1>
+      </div>
+      <span>
+        {{ project.description }}
+      </span>
+      <div>
+        <img :src="project.image" :alt="project.title" />
+      </div>
+      <div>
+        {{ project.type.title }}
+      </div>
+      <div v-for="elem in project.technologies">
+        {{ elem.title }}
+      </div>
+      <span>
+        {{ project.date }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-container {
+  display: flex;
+  width: 300px;
+}
+</style>
